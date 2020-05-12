26cc4918b002f0fb93d00522734a96cf9153ad7d
rayhanrizz <57081483+rayhanrizz@users.noreply.github.com>
2020-05-12 13:52:40 +0700

    Bottom Sheet

---
+++ BottomSheet/app/src/main/java/com/example/bottomsheet/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bottomsheet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_sheet_layout.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_layout)
+        btnBottomSheet.setOnClickListener {
+            if (bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+            } else {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED)
+            }
+        }
+    }
+}
